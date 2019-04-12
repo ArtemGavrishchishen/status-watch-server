9439bb14202812f9282f63502af553f2b42a005e
Artem Gavrishchishen <artem.gavrishchishen@gmail.com>
2019-04-12 21:07:37 +0300

    fix

--- src/controller/routes/homePage.js
+++ src/controller/routes/homePage.js
@@ -1,17 +1,17 @@
-const path = require("path");
-const fs = require("fs");
+const path = require('path');
+const fs = require('fs');
 
-const filePath = path.join(__dirname, "../../../", "db", "db.json");
+const filePath = path.join(__dirname, '../../../', 'data', 'db.json');
 
 const getHomePageInfo = (request, response) => {
-  const dbJson = fs.readFileSync(filePath, "utf8");
+  const dbJson = fs.readFileSync(filePath, 'utf8');
   const data = JSON.parse(dbJson);
 
-  response.set("Content-Type", "application/json");
-  response.header("Access-Control-Allow-Origin", "*");
+  response.set('Content-Type', 'application/json');
+  response.header('Access-Control-Allow-Origin', '*');
   response.status(200);
   response.json({
-    status: "success",
+    status: 'success',
     shares: data.shares,
     watches: data.watches
   });
--- src/server.js
+++ src/server.js
@@ -1,24 +1,26 @@
-const bodyParser = require("body-parser");
-const morgan = require("morgan");
+const bodyParser = require('body-parser');
+const express = require('express');
+const morgan = require('morgan');
 
-const { app, routes } = require("./controller");
+const { app, routes } = require('./controller');
 
 const errorHandler = (req, res, next) => {
-  res.status(404).send("Page not found");
+  res.status(404).send('Page not found');
   next();
 };
 
 const startServer = port => {
   app
+    .use(express.static('public'))
     .use(bodyParser.urlencoded({ extended: false }))
     .use(bodyParser.json())
-    .use(morgan("dev"))
-    .use("/", routes)
+    .use(morgan('dev'))
+    .use('/', routes)
     .use(errorHandler);
 
   app.listen(port);
 
-  console.log("Server: http://localhost:" + port);
+  console.log('Server: http://localhost:' + port);
 };
 
 module.exports = startServer;
